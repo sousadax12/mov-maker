abf9f1e4d7249654a9ecb092bd06889a3ee516ae
Joao Sousa <js@void.pt>
2018-09-20 23:40:04 +0100

    no message

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AboutModule,
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
